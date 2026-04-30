1c23523e4f64cf85b27354a4e1eca0ca9f27a5d6
m0m0se763 <113299896+m0m0se763@users.noreply.github.com>
2026-04-30 19:49:11 +0900

    add player system

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+players = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        if name:
+            players.append(name)
+    return render_template("index.html", players=players)
+
+if __name__ == "__main__":
+    app.run()
