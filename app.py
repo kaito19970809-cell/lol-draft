0e5fa08bcf544a72027fb64331a9e03346ab21aa
m0m0se763 <113299896+m0m0se763@users.noreply.github.com>
2026-04-30 19:45:35 +0900

    add html

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Render!"
-
-if __name__ == "__main__":
-    app.run()
+    return render_template("index.html")
